a37f617ebb1e494c5889a2a9930c32ee2516f8f9
Auliya <auliya.az@mhs.itenas.ac.id>
2024-12-24 15:01:02 +0700

    Add functionality for substraction number

--- TM-12-GitAritmatika/src/tm/pkg12/gitaritmatika/BilanganInterface.java
+++ TM-12-GitAritmatika/src/tm/pkg12/gitaritmatika/BilanganInterface.java
@@ -10,4 +10,9 @@ package tm.pkg12.gitaritmatika;
  */
 public interface BilanganInterface {
     
+    public double add(Bilangan bilangan);
+    public double sub(Bilangan bilangan);
+    public double mult(Bilangan bilangan);
+    public double div(Bilangan bilangan);
+    
 }
